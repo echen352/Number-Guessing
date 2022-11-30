6aaa677e184f67c7d9dcea4a52f6c13d619d645e
ELLIS CHEN <ellischen352@gmail.com>
2022-11-30 14:50:29 -0800

    Slight changes to FSM

--- fsm.sv
+++ fsm.sv
@@ -1,10 +1,11 @@
   module FIFO_fsm(input logic clk, restart, 		// restart is reset button, change to sw
-					   input logic [2:0]guess, round,
+					   input logic [2:0]incorrect_guesses, round,
 					   input logic [6:0]timer,
 						input logic comfirmButton,		// comfirm the input number
 					  //output logic [6:0]Max_timer,
-					  output logic [2:0]Max_guess, 
-					  output logic [1:0]Max_digit
+					  output logic [2:0]Max_incorrect_guesses, 
+					  output logic [1:0]Max_digit,
+					  output logic diff_timer
 						);
 			   
 	typedef enum logic [2:0] {diff1, diff2, diff3, gameover, win} stateType;
@@ -21,10 +22,11 @@
 	begin	
 			case(presentState)
 				diff1: begin
+						diff_timer <= 1;
 					if(comfirmButton)begin
-						if( timer > 0 && guess <= 3 && round > 3) begin
+						if( timer > 0 && incorrect_guesses <= 2 && round > 4 ) begin
 							nextState <= diff2;
-						end else if ( timer == 0 && guess > 3 && round <= 3) begin
+						end else if ( timer == 0 || incorrect_guesses > 2 ) begin
 								nextState <= gameover;
 						end else
 								nextState <= diff1;
@@ -33,10 +35,11 @@
 				end
 				
 				diff2: begin
+						diff_timer <= 2;
 					if(comfirmButton)begin				
-						if( timer > 0 && guess <= 4 && round > 3) begin
+						if( timer > 0 && incorrect_guesses <= 3 && round > 4 ) begin
 							nextState <= diff3;
-						end else if ( timer == 0 && guess > 4 && round <= 3) begin
+						end else if ( timer == 0 || incorrect_guesses > 3 ) begin
 								nextState <= gameover;
 						end else
 								nextState <= diff2;
@@ -45,10 +48,11 @@
 				end
 				
 				diff3: begin
+						diff_timer <= 3;
 					if(comfirmButton)begin				
-						if( timer > 0 && guess <= 5 && round > 3) begin
+						if( timer > 0 && incorrect_guesses <= 4 && round > 4 ) begin
 							nextState <= win;
-						end else if ( timer == 0 && guess > 5 && round <= 3) begin
+						end else if ( timer == 0 || incorrect_guesses > 4 ) begin
 								nextState <= gameover;
 						end else
 								nextState <= diff3;
@@ -57,6 +61,7 @@
 				end	
 				
 				win: begin
+					diff_timer <= 0;
 					if(comfirmButton)begin
 						if(restart)
 							nextState <= diff1;
@@ -67,6 +72,7 @@
 				end
 				
 				gameover: begin
+						diff_timer <= 0;
 					if(comfirmButton)begin
 							if(restart)
 								nextState <= diff1;
@@ -88,27 +94,27 @@
 		case(presentState)
 			diff1: begin
 				//Max_timer <= 30;
-				Max_guess <= 3;
+				Max_incorrect_guesses <= 3;
 				Max_digit <= 1;
 			end
 			diff2: begin
 				//Max_timer <= 60;
-				Max_guess <= 4;
+				Max_incorrect_guesses <= 4;
 				Max_digit <= 2;
 			end
 			diff3: begin
 				//Max_timer <= 90;
-				Max_guess <= 5;
+				Max_incorrect_guesses <= 5;
 				Max_digit <= 3;
 			end
 			gameover: begin
 				//Max_timer <= 0;
-				Max_guess <= 0;
+				Max_incorrect_guesses <= 0;
 				Max_digit <= 0;
 			end
 			win: begin
 				//Max_timer <= 0;
-				Max_guess <= 0;
+				Max_incorrect_guesses <= 0;
 				Max_digit <= 0;
 			end
 		endcase
